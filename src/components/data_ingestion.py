3fa8eda3426d62ab97c9582b17d6a44280851c3a
Ranjeet-Swami <ranjitswami406@gmail.com>
2024-01-10 17:41:22 +0530

    data transmission done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import dataTransformationConfig
 
 @dataclass
 class dataingestionConfig:
@@ -28,6 +30,7 @@ class dataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info('train test split initiated')
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
@@ -36,6 +39,7 @@ class dataIngestion:
             return(self.ingestion_config.train_data_path,
                    self.ingestion_config.test_data_path
                    )
+            
 
 
 
@@ -45,4 +49,10 @@ class dataIngestion:
 
 if __name__=="__main__":    
     obj=dataIngestion()
-    obj.initiate_data_ingestion()
+    train_path,test_path = obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_path,test_path)
+
+
+
+
