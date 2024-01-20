f286814a363e90a758ebdb9d52c8469f975bf19e
Ranjeet-Swami <ranjitswami406@gmail.com>
2024-01-20 19:16:47 +0530

    deleted apppy file for deployement

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -71,10 +71,10 @@ class DataTransformation:
                numerical_columns=["writing_score","reading_score"]
 
                input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-               target_feature_train_df=train_df=train_df[target_column_name]
+               target_feature_train_df=train_df[target_column_name]
                input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
                
-               target_feature_test_df=test_df=test_df[target_column_name]
+               target_feature_test_df=test_df[target_column_name]
 
 
                logging.info(f"applying preprocessing obj on training dataframe and testing dataframe.")
